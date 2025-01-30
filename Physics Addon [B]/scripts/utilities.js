77c9dc062e580c2e8f7a61f6b100b9420f1e1538
Yasser Null <162287858+YasserNull@users.noreply.github.com>
2025-01-30 20:08:53 +0100

    Update utilities.js

--- Physics Addon [B]/scripts/utilities.js
+++ Physics Addon [B]/scripts/utilities.js
@@ -5,3 +5,16 @@ export function subtract(location, vector3) {
         z: location.z - vector3.z
     };
 }
+
+export function distance(vector1, vector2) {
+    return Math.sqrt((vector1.x - vector2.x) ** 2 + (vector1.y - vector2.y) ** 2 + (vector1.z - vector2.z) ** 2);
+}
+
+export function getDirection(vector1, vector2) {
+    let dist = distance(vector1, vector2);
+    return {
+        x: (vector2.x - vector1.x) / dist,
+        y: (vector2.y - vector1.y) / dist,
+        z: (vector2.z - vector1.z) / dist
+    };
+}
